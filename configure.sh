e8943546bfae4faa1df828f8bb25f63f5a1fcb8c
Kia Rahmani <rahmank@purdue.edu>
2018-03-14 21:47:10 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -9,6 +9,7 @@ read public
 echo "Enter your private ip: "
 read private
 
+sudo rm -r -f /opt/apache-cassandra-3.11.2/data/*
 sed s/SADDRESS/$seed/ conf/cassandra_template.yaml > temp1
 sed s/LADDRESS/$private/ temp1 > temp2
 sed s/BCADDRESS/$public/ temp2 > cassandra.yaml
