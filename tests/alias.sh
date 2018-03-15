59caff924ebbed7cae9a984f18ef34c233b97470
Ubuntu <ubuntu@ip-172-31-45-98.us-west-2.compute.internal>
2018-03-15 18:45:46 +0000

    tmp

--- tests/alias.sh
+++ tests/alias.sh
@@ -7,10 +7,10 @@ echo "Enter the transaction kind [NoTxn|RC|MAV|RR]:"
 read txn
 
 
-alias b="./LWW_txn --kind Broker --brokerAddr $broker --txnKind $txn"
-alias m="make clean && make LWW_txn"
+alias b="clear && ./LWW_txn --kind Broker --brokerAddr $broker --txnKind $txn"
+alias m="make clean && make LWW_txn && clear"
 alias d="./LWW_txn --kind Drop --txnKind $txn && ./LWW_txn --kind Create --txnKind $txn"
 alias dd="./LWW_txn --kind Create --txnKind $txn"
 
-alias s="./LWW_txn --kind Server --rtsArgs “-N2” --brokerAddr $broker --txnKind $txn"
+alias s="clear && ./LWW_txn --kind Server --rtsArgs “-N2” --brokerAddr $broker --txnKind $txn"
 
